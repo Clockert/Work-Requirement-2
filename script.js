0666d19fb834343e1396a331efe49a1f5abcaf68
Clockert <charlotteikn@gmail.com>
2025-02-18 15:13:37 +0100

    fixed javascript, added button
    added subscribe button, removed the modal pop-up on img hoover. added auto popup after two seconds

--- script.js
+++ script.js
@@ -1,17 +1,30 @@
-var saturnImage = document.querySelector("img");
-saturnImage.onmouseover = function () {
-  var modal = document.getElementById("myModal");
-  modal.style.display = "block";
+// Select the modal and button elements
+var modal = document.getElementById("myModal");
+var closeBtn = document.getElementsByClassName("close")[0];
+var actionButton = document.getElementById("SubscribeButton"); // New button to trigger the modal
+
+// Function to show the modal
+function showModal() {
+  if (modal.style.display !== "block") {
+    modal.style.display = "block";
+  }
+}
+
+// Show the modal after a delay when the page loads
+window.onload = function () {
+  setTimeout(showModal, 2000); // Show modal after 2 seconds
 };
 
-var closeBtn = document.getElementsByClassName("close")[0];
+// Show the modal when the button is clicked
+actionButton.addEventListener("click", showModal);
+
+// Close the modal when the close button is clicked
 closeBtn.onclick = function () {
-  var modal = document.getElementById("myModal");
   modal.style.display = "none";
 };
 
+// Close the modal when clicking outside of it
 window.onclick = function (event) {
-  var modal = document.getElementById("myModal");
   if (event.target == modal) {
     modal.style.display = "none";
   }
